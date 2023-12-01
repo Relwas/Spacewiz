0598dde731d0331ad931005c473f4377b0bcefe9
relwas <imrelwas@gmail.com>
2023-12-01 18:22:35 +0500

    Initial Commit

---
+++ SpaceWiz1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceWiz1
+//
+//  Created by relwas on 01/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
